66e727682fee41b4412b49c48789d1af3168c720
jokesfor300 <fyodormorozko95@gmail.com>
2021-02-12 20:23:39 +0300

    Use context manager for progress bar.

--- produce_data.py
+++ produce_data.py
@@ -41,29 +41,28 @@ fname = 'dataset.npz'
 if __name__ == '__main__':
     samples = dict(D=[], R=[])
     try:
-        pbar = tqdm(total=batch_size, leave=False)
         i = 0
-        while True:
-            D = np.random.random_sample(n_grating_layers)
-            gr = Grating(epsilon_Si, epsilon_SiO2, D)
-            ps = gr.propagator(freqs)
-            ts = np.array(list(map(operator_t, ps)))
-            i += 1
-            pbar.update(1)
-            samples['D'].append(D)
-            samples['R'].append(np.abs(ts[:, 0, 0]) ** 2)
-            if i == batch_size:
-                save_samples(fname, samples)
-                samples = dict(D=[], R=[])
-                gc.collect()
-                pbar.reset()
-                i = 0
+        with tqdm(total=batch_size, leave=False) as pbar:
+            while True:
+                D = np.random.random_sample(n_grating_layers)
+                gr = Grating(epsilon_Si, epsilon_SiO2, D)
+                ps = gr.propagator(freqs)
+                ts = np.array(list(map(operator_t, ps)))
+                i += 1
+                pbar.update(1)
+                samples['D'].append(D)
+                samples['R'].append(np.abs(ts[:, 0, 0]) ** 2)
+                if i == batch_size:
+                    save_samples(fname, samples)
+                    samples = dict(D=[], R=[])
+                    gc.collect()
+                    pbar.reset()
+                    i = 0
     except KeyboardInterrupt:
         print('Interrupting calculation...')
         answer = input('Do you want to save calculated data? y/[n]: ')
         if answer == 'y':
             save_samples(fname, samples)
     finally:
-        pbar.close()
         print('Exiting script')
         exit(0)
